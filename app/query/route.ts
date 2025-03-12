e34e41a0b461c2282bbf8c0faecb280d31f0dc79
lekan <lekanm715@gmail.com>
2025-03-12 17:20:12 +0100

    initial commits

--- app/query/route.ts
+++ app/query/route.ts
@@ -16,8 +16,9 @@
 export async function GET() {
   return Response.json({
     message:
-      'Uncomment this file and remove this line. You can delete this file when you are finished.',
+      "Uncomment this file and remove this line. You can delete this file when you are finished.",
   });
+
   // try {
   // 	return Response.json(await listInvoices());
   // } catch (error) {
